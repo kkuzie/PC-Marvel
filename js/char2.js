622f1cf18d4f9726d7ad84796f38837227618b1d
Karen Kuzi <kkuzii@yahoo.it>
2022-06-23 06:35:33 -0700

    attribution/charDesc if none/add series

--- js/char2.js
+++ js/char2.js
@@ -1,34 +1,52 @@
 var baseUrl = "https://gateway.marvel.com:443/v1/public/characters"
-        var apiKey = "apikey=d9758292826793baa47319d440b91d8a&hash=0137b844f3396fccaf1be6229cf376e4"
+var apiKey = "apikey=d9758292826793baa47319d440b91d8a&hash=0137b844f3396fccaf1be6229cf376e4"
 
-        var allChars = "?ts=1&"
-        var charID = "/{characterId}";
-        var stories = "/stories";
+var getChar = JSON.parse(localStorage.getItem('localCharID'));
+console.log("This is the localstorage "+ getChar)
+var allChars = "?ts=1&"
+var charID = "/"+getChar;
+var stories = "/stories";
 
-        var charUrl = baseUrl + allChars + apiKey;
-        var charStoriesUrl = baseUrl + charID + stories;
+var charUrl = baseUrl + charID + allChars + apiKey;
+var charStoriesUrl = baseUrl + charID + stories;
 
+var attribution = document.getElementById('attribution');
 
-$.getJSON(charUrl, function(data){
+$.getJSON(charUrl, function (data) {
     console.log(data);
 
-    var element = data.data.results;
+    var element = data.data.results[0];
     console.log(element);
-    var charImg = element[1].thumbnail.path + "/standard_fantastic." + element[0].thumbnail.extension;
+
+    var charImg = element.thumbnail.path + "/standard_fantastic." + element.thumbnail.extension;
     console.log(charImg);
 
-    var charName = element[1].name;
-    var charDesc = element[1].description;
+    var charName = element.name;
+    var charDesc = element.description;
+    var charStory = element.stories.items[0].resourceURI;
+    console.log(charStory);
 
     $(".character-img").attr("src", charImg);
     $(".character-name").append(charName);
-    $(".character-desc").append(charDesc);
 
+    if (charDesc == "") {
+        $(".character-desc").append("No description available at this time. Check back again!");
+    } else {
+        $(".character-desc").append(charDesc);
+    };
 
+    for(var i=0; i<3 ;i++) {
+    var charSeries = element.series.items[i].name;
+    console.log(charSeries);
+    var idIndex = i +1;
+    $("#series-"+idIndex).append(charSeries);
+    // $("#series-"+([i]+1)).append(charSeries);
 
+    };
 
+    attribution.innerHTML = data.attributionHTML;
 });
 
-$.getJSON(charStoriesUrl, function(data){
-    console.log(data);
-});
+// $.getJSON(charStoriesUrl, function (data) {
+//     console.log(data);
+// });
